1e5f1c9fa1ad26db3d778146588c5acdc87b0f64
rocosov <ps2carlos@gmail.com>
2019-06-05 15:26:00 -0400

    listado eventos en index

--- public/index.php
+++ public/index.php
@@ -134,16 +134,13 @@ require_once("header.php");
                 <a class="btn btn-outline-primary btn-block" href="'.$resultados['alias'].'">Leer mas..</a>
             </div>
           </div>
-
-
-
                ';}
           else {echo "<br/>No hay más datos!!! <br/>";}
           }
-        include("../php/conexion.php");
+          include("../php/conexion.php");
 
           $tildes = $mysqli->query("SET NAMES 'utf8'"); //Para que se muestren las tildes correctamente
-          $result = mysqli_query($mysqli, "SELECT  * from contenidos c where c.tipo='POST' ORDER BY c.fecha_publicacion desc limit 3");
+          $result = mysqli_query($mysqli, "SELECT  * from contenidos c where c.tipo='POST' ORDER BY c.fecha desc limit 3");
           while ($fila = mysqli_fetch_array($result)){
           mostrarDatos($fila);
           }
@@ -177,7 +174,7 @@ require_once("header.php");
    			<div class="event_items">
 
    				<!-- Event Item -->
-   				<div class="row event_item">
+   				<!-- <div class="row event_item">
    					<div class="col">
    						<div class="row d-flex flex-row align-items-end">
 
@@ -204,12 +201,49 @@ require_once("header.php");
 
    						</div>
    					</div>
-   				</div>
+   				</div> -->
 
+          <?php
+          include("../php/conexion.php");
 
+          $tildes = $mysqli->query("SET NAMES 'utf8'"); //Para que se muestren las tildes correctamente
+          $result = mysqli_query($mysqli, "SELECT  * from contenidos c where c.tipo='EVENTO' ORDER BY c.fecha desc limit 10");
+          while ($fila = mysqli_fetch_array($result))
+          {
+          echo '   				<!-- Event Item -->
+             				<div class="row event_item">
+             					<div class="col">
+             						<div class="row d-flex flex-row align-items-end">
 
+             							<div class="col-lg-2 order-lg-1 order-2">
+             								<div class="event_date d-flex flex-column align-items-center justify-content-center">
+             									<div class="event_day">07</div>
+             									<div class="event_month">January</div>
+             								</div>
+             							</div>
+
+             							<div class="col-lg-6 order-lg-2 order-3">
+             								<div class="event_content">
+             									<div class="event_name"><a class="trans_200" href="'.$fila['alias'].'">Student Festival</a></div>
+             									<div class="event_location">Grand Central Park</div>
+             									<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+             								</div>
+             							</div>
+
+             							<div class="col-lg-4 order-lg-3 order-1">
+             								<div class="event_image">
+             									<img src="../images/event_1.jpg" alt="https://unsplash.com/@theunsteady5">
+             								</div>
+             							</div>
+
+             						</div>
+             					</div>
+             				</div>';
+          }
+          mysqli_free_result($result);
+          mysqli_close($mysqli);
+          ?>
    			</div>
-
    		</div>
    	</div>
 
